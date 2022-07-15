fe261a07944445b1ce6d108b836f556408207f82
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-07-15 17:42:50 +0930

    Code the script for rating buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
+let ratingChoice;
+
 const tyCard = document.querySelector(".thank-you-state");
+const ratingCard = document.querySelector(".rating-state");
 tyCard.style["display"] = "none";
+
+const ratingButtons = document.querySelectorAll(".rating-selector");
+ratingButtons.forEach((ratingButton) => {
+  ratingButton.addEventListener("click", () => {
+    ratingChoice = ratingButton.id;
+  });
+});
